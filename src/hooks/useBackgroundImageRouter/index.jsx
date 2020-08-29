ece03ea60bfff86171e9e45f87740c21cab37084
Wayne <wayne.c.griffiths@gmail.com>
2020-08-29 19:11:40 +0800

    revert last change with useLocation

--- src/hooks/useBackgroundImageRouter/index.jsx
+++ src/hooks/useBackgroundImageRouter/index.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 
 // css for the html body
 const pageStyle = {
@@ -13,22 +12,21 @@ const pageStyle = {
  */
 export default backgroundImageMap => {
 
-    // const [ location ] = useState(window.location.pathname);
-    const location = useLocation();
+    const [ location ] = useState(window.location.pathname);
     const [ imageMap ] = useState(backgroundImageMap);
     const [ bodyStyle ] = useState(pageStyle);
 
     // Changes the body css everytime the router switches pages
     useEffect(() => {
-        if(typeof(imageMap[location.pathname]) === 'object') {
+        if(typeof(imageMap[location]) === 'object') {
 
             //image
-            document.body.style.backgroundImage = `url(${imageMap[location.pathname].image})`
+            document.body.style.backgroundImage = `url(${imageMap[location].image})`
 
             // position options
-            if(imageMap[location.pathname].position)  document.body.style.backgroundPosition = imageMap[location].position;  
-            if(imageMap[location.pathname].positionX) document.body.style.backgroundPositionX = imageMap[location].positionX;
-            if(imageMap[location.pathname].positionY) document.body.style.backgroundPositionY = imageMap[location].positionY;
+            if(imageMap[location].position)  document.body.style.backgroundPosition = imageMap[location].position;  
+            if(imageMap[location].positionX) document.body.style.backgroundPositionX = imageMap[location].positionX;
+            if(imageMap[location].positionY) document.body.style.backgroundPositionY = imageMap[location].positionY;
 
         } else {
             // else just serve and image and center it as default
